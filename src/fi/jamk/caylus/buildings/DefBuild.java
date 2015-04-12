7b5d5ec4401618978cb3636ac84f797580af545e
Mytechnique <lehtosaarih@gmail.com>
2015-04-12 19:01:08 +0300

    Building
    Conflicts:
    	build/classes/fi/jamk/caylus/DefBuild.class

--- src/fi/jamk/caylus/buildings/DefBuild.java
+++ src/fi/jamk/caylus/buildings/DefBuild.java
@@ -1,60 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fi.jamk.caylus.buildings;
 import fi.jamk.caylus.Resources;
 
-/**
- *
- * @author Omistaja
- */
-public class DefBuild extends Resources{
+/** 
+ * 
+ * @author Mythe 
+ */ 
+
+public class DefBuild extends Building{
     
     public DefBuild(){
-        super(0,0,0,0,0,0,0,0);
-    }
-   
-    public int defGoldMine(){
-        DefBuild defGoldMine = new DefBuild();
-        defGoldMine.addGold(1);
-       
-        return defGoldMine.getGold();
+        super();
     }
     
-     public int defTrader(String trade){
-        DefBuild defTrader = new DefBuild();
-       
-        switch(defTrader.toString()){
-           
-            case "food":
-                defTrader.subMoney(2);
-                defTrader.addFood(1);
-                break;
-           
-            case "wood":
-                defTrader.subMoney(2);
-                defTrader.addWood(1);
-                break;
-               
-            case "stone":
-                defTrader.subMoney(2);
-                defTrader.addStone(1);
-                break;
-               
-            case "silk": defTrader.subMoney(2);
-                defTrader.addSilk(1);
-                break;
-               
-        }return defTrader.getMoney() + defTrader.getFood() + defTrader.getWood() + defTrader.getStone() + defTrader.getSilk();
-       
-    }
-
-    public int defPermit(){
-        DefBuild defPermit = new DefBuild();
-        defPermit.setDefBuild(true);
+    public void defGoldMine(){
         
-        return defPermit.defPermit();
+    }
+      public Resources trade(String opt){
+          this.cost.addMoney(2);
+        switch(opt){
+            
+            case "food": 
+                reward.addFood(1);
+                break;
+            
+            case "wood": 
+                reward.addWood(1);
+                break;
+                
+            case "stone": 
+                reward.addStone(1);
+                break;
+                
+            case "silk": 
+                reward.addSilk(1);
+                break;
+                
+        }
+        return this.reward;
+        
     }
 }
