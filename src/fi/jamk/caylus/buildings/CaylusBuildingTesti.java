a3b65f6ca93110618d17450b7d417c3f2e253e8c
Mytechnique <lehtosaarih@gmail.com>
2015-04-12 20:29:46 +0300

    CaylusBuildingTestAdd
    Few lines added to test

--- src/fi/jamk/caylus/buildings/CaylusBuildingTesti.java
+++ src/fi/jamk/caylus/buildings/CaylusBuildingTesti.java
@@ -12,20 +12,18 @@ public class CaylusBuildingTesti {
     public static void main (String args[]){
         
       Player player1 = new Player("Hessu");
-        
-
       
-      
-     // DefBuild trader = new DefBuild();
-//      trader.trade("food");
+      DefBuild trader = new DefBuild();
+      trader.trade("food");
       
       player1.addAut(7);
       
       player1.subGold(5); 
         
       System.out.println(player1.getMoney()); // Rahat nyt
-    //  System.out.println(player1.subMoney(trader.cost.getMoney())); // vähennetään DefBuild "trader" olion sisältämästä "cost" nimisen Resource olion raha muuttujan arvo =/= 2
-        System.out.println(player1);
+      System.out.println(player1.subMoney(trader.cost.getMoney())); // vähennetään DefBuild "trader" olion sisältämästä "cost" nimisen Resource olion raha muuttujan arvo =/= 2
+        System.out.println(player1.addFood(trader.reward.getFood()));
+        System.out.println(player1.addSilk(trader.reward.getSilk()));
         
       System.out.println(player1.resourcesInfo()); 
       
